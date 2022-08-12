d083ca4ef74fd8643db9aef0b72f5872e3cba716
Rishma Murugan <rishma2000@gmail.com>
2022-08-12 13:36:53 -0700

    Initial postgres connection working for users table

---
+++ server.py
@@ -0,0 +1,45 @@
+from venv import create
+import psycopg2
+import psycopg2.extras
+
+hostname = "localhost"
+database = "chatroom"
+user = "postgres"
+password="test1234"
+port_id = 5432
+conn = None
+
+try:
+    with psycopg2.connect(
+        host = hostname,
+        dbname = database,
+        user = user,
+        password = password,
+        port = port_id) as conn: 
+    
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+            cur.execute('DROP TABLE IF EXISTS users')
+
+            create_script = ''' 
+                CREATE TABLE IF NOT EXISTS users (
+                    id  int PRIMARY KEY,
+                    username varchar(40) NOT NULL,
+                    password varchar(40) NOT NULL
+                )
+            '''
+            cur.execute(create_script)
+
+            insert_script = 'INSERT INTO users (id, username, password) VALUES (%s, %s, %s)'
+            insert_value = (1, 'ali', 'pw1')
+            cur.execute(insert_script, insert_value)
+
+            cur.execute('SELECT * FROM USERS')
+            for record in cur.fetchall():
+                print(record['id'], record['username'])
+
+except Exception as error:
+    print(error)
+
+finally:
+    if conn is not None:
+        conn.close()
